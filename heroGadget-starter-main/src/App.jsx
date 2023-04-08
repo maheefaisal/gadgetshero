9816b3b42eec15501054a2aeed45716b84bbfb08
Ehfaz Mahee <mahee504@gmail.com>
2023-04-08 23:02:53 +0600

    3rd

--- heroGadget-starter-main/src/App.jsx
+++ heroGadget-starter-main/src/App.jsx
@@ -1,13 +1,17 @@
+import { Outlet } from "react-router-dom";
 import About from "./components/About";
 import Header from "./components/Header";
 import Home from "./components/Home";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
     <>
       <Header></Header>
-      <Home></Home>
-      <About></About>
+      <div className="min-h-[calc(100vh-137px)]">
+        <Outlet></Outlet>
+      </div>
+      <Footer></Footer>
     </>
   );
 };
